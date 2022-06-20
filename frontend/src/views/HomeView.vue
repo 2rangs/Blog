433edb2a7a29cba275128e237aa2d41eac5e6dd4
2-rangs <2_rang25@naver.com>
2022-06-20 18:17:36 +0900

    test

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,18 +1,15 @@
 <template>
-  <div class="home">
-    <!-- <img alt="Vue logo" src="../assets/logo.png"> -->
-    <HelloWorld/>
-  </div>
+    <div class="home">
+        <!-- <img alt="Vue logo" src="../assets/logo.png"> -->
+        <HelloWorld/>
+    </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from '../components/HelloWorld.vue'; 
+    import {defineComponent} from 'vue';
+    import HelloWorld from '../components/HelloWorld.vue';
 
-export default defineComponent({
-  name: 'HomeView',
-  components: {
-    HelloWorld,
-  },
-});
+    export default defineComponent({name: 'HomeView', components: {
+            HelloWorld
+        }});
 </script>
